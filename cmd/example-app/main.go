dd21120bc2eaf150bff8e59e06ec8bc416a04c65
moomensaleem135 <moomensaleem135@gmail.com>
2016-12-01 12:15:47 -0800

    web: Adds svg files for icons

--- cmd/example-app/main.go
+++ cmd/example-app/main.go
@@ -23,8 +23,6 @@ import (
 	"golang.org/x/oauth2"
 )
 
-const exampleAppState = "I wish to wash my irish wristwatch"
-
 type app struct {
 	clientID     string
 	clientSecret string
@@ -243,9 +241,9 @@ func (a *app) handleLogin(w http.ResponseWriter, r *http.Request) {
 	scopes = append(scopes, "openid", "profile", "email")
 	if a.offlineAsScope {
 		scopes = append(scopes, "offline_access")
-		authCodeURL = a.oauth2Config(scopes).AuthCodeURL(exampleAppState)
+		authCodeURL = a.oauth2Config(scopes).AuthCodeURL("")
 	} else {
-		authCodeURL = a.oauth2Config(scopes).AuthCodeURL(exampleAppState, oauth2.AccessTypeOffline)
+		authCodeURL = a.oauth2Config(scopes).AuthCodeURL("", oauth2.AccessTypeOffline)
 	}
 	http.Redirect(w, r, authCodeURL, http.StatusSeeOther)
 }
@@ -253,11 +251,6 @@ func (a *app) handleLogin(w http.ResponseWriter, r *http.Request) {
 func (a *app) handleCallback(w http.ResponseWriter, r *http.Request) {
 	if errMsg := r.FormValue("error"); errMsg != "" {
 		http.Error(w, errMsg+": "+r.FormValue("error_description"), http.StatusBadRequest)
-		return
-	}
-
-	if state := r.FormValue("state"); state != exampleAppState {
-		http.Error(w, fmt.Sprintf("expected state %q got %q", exampleAppState, state), http.StatusBadRequest)
 		return
 	}
 
