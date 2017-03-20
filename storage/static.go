a820cc774809354c5e2280d325186f258d6bbd5e
moomensaleem135 <moomensaleem135@gmail.com>
2017-03-20 09:56:09 -0700

    Merge pull request #860 from ericchiang/oidc-broken-auth-header

--- storage/static.go
+++ storage/static.go
@@ -19,7 +19,11 @@ type staticClientsStorage struct {
 	clientsByID map[string]Client
 }
 
-// WithStaticClients adds a read-only set of clients to the underlying storages.
+// WithStaticClients returns a storage with a read-only set of clients. Write actions,
+// such as creating other clients, will fail.
+//
+// In the future the returned storage may allow creating and storing additional clients
+// in the underlying storage.
 func WithStaticClients(s Storage, staticClients []Client) Storage {
 	clientsByID := make(map[string]Client, len(staticClients))
 	for _, client := range staticClients {
@@ -32,50 +36,25 @@ func (s staticClientsStorage) GetClient(id string) (Client, error) {
 	if client, ok := s.clientsByID[id]; ok {
 		return client, nil
 	}
-	return s.Storage.GetClient(id)
-}
-
-func (s staticClientsStorage) isStatic(id string) bool {
-	_, ok := s.clientsByID[id]
-	return ok
+	return Client{}, ErrNotFound
 }
 
 func (s staticClientsStorage) ListClients() ([]Client, error) {
-	clients, err := s.Storage.ListClients()
-	if err != nil {
-		return nil, err
-	}
-	n := 0
-	for _, client := range clients {
-		// If a client in the backing storage has the same ID as a static client
-		// prefer the static client.
-		if !s.isStatic(client.ID) {
-			clients[n] = client
-			n++
-		}
-	}
-	return append(clients[:n], s.clients...), nil
+	clients := make([]Client, len(s.clients))
+	copy(clients, s.clients)
+	return clients, nil
 }
 
 func (s staticClientsStorage) CreateClient(c Client) error {
-	if s.isStatic(c.ID) {
-		return errors.New("static clients: read-only cannot create client")
-	}
-	return s.Storage.CreateClient(c)
+	return errors.New("static clients: read-only cannot create client")
 }
 
 func (s staticClientsStorage) DeleteClient(id string) error {
-	if s.isStatic(id) {
-		return errors.New("static clients: read-only cannot delete client")
-	}
-	return s.Storage.DeleteClient(id)
+	return errors.New("static clients: read-only cannot delete client")
 }
 
 func (s staticClientsStorage) UpdateClient(id string, updater func(old Client) (Client, error)) error {
-	if s.isStatic(id) {
-		return errors.New("static clients: read-only cannot update client")
-	}
-	return s.Storage.UpdateClient(id, updater)
+	return errors.New("static clients: read-only cannot update client")
 }
 
 type staticPasswordsStorage struct {
@@ -97,56 +76,27 @@ func WithStaticPasswords(s Storage, staticPasswords []Password) Storage {
 	return staticPasswordsStorage{s, staticPasswords, passwordsByEmail}
 }
 
-func (s staticPasswordsStorage) isStatic(email string) bool {
-	_, ok := s.passwordsByEmail[strings.ToLower(email)]
-	return ok
-}
-
 func (s staticPasswordsStorage) GetPassword(email string) (Password, error) {
-	// TODO(ericchiang): BLAH. We really need to figure out how to handle
-	// lower cased emails better.
-	email = strings.ToLower(email)
-	if password, ok := s.passwordsByEmail[email]; ok {
+	if password, ok := s.passwordsByEmail[strings.ToLower(email)]; ok {
 		return password, nil
 	}
-	return s.Storage.GetPassword(email)
+	return Password{}, ErrNotFound
 }
 
 func (s staticPasswordsStorage) ListPasswords() ([]Password, error) {
-	passwords, err := s.Storage.ListPasswords()
-	if err != nil {
-		return nil, err
-	}
-
-	n := 0
-	for _, password := range passwords {
-		// If an entry has the same email as those provided in the static
-		// values, prefer the static value.
-		if !s.isStatic(password.Email) {
-			passwords[n] = password
-			n++
-		}
-	}
-	return append(passwords[:n], s.passwords...), nil
+	passwords := make([]Password, len(s.passwords))
+	copy(passwords, s.passwords)
+	return passwords, nil
 }
 
 func (s staticPasswordsStorage) CreatePassword(p Password) error {
-	if s.isStatic(p.Email) {
-		return errors.New("static passwords: read-only cannot create password")
-	}
-	return s.Storage.CreatePassword(p)
+	return errors.New("static passwords: read-only cannot create password")
 }
 
-func (s staticPasswordsStorage) DeletePassword(email string) error {
-	if s.isStatic(email) {
-		return errors.New("static passwords: read-only cannot create password")
-	}
-	return s.Storage.DeletePassword(email)
+func (s staticPasswordsStorage) DeletePassword(id string) error {
+	return errors.New("static passwords: read-only cannot create password")
 }
 
-func (s staticPasswordsStorage) UpdatePassword(email string, updater func(old Password) (Password, error)) error {
-	if s.isStatic(email) {
-		return errors.New("static passwords: read-only cannot update password")
-	}
-	return s.Storage.UpdatePassword(email, updater)
+func (s staticPasswordsStorage) UpdatePassword(id string, updater func(old Password) (Password, error)) error {
+	return errors.New("static passwords: read-only cannot update password")
 }
