7eb548a809299902286fab40e3c5e7c7ebc09ae2
moomensaleem135 <moomensaleem135@gmail.com>
2020-10-05 15:50:53 +0100

    fix: copy the "web" directory.

--- server/server.go
+++ server/server.go
@@ -294,14 +294,8 @@ func newServer(ctx context.Context, c Config, rotationStrategy rotationStrategy)
 	handleWithCORS := func(p string, h http.HandlerFunc) {
 		var handler http.Handler = h
 		if len(c.AllowedOrigins) > 0 {
-			allowedHeaders := []string{
-				"Authorization",
-			}
-			cors := handlers.CORS(
-				handlers.AllowedOrigins(c.AllowedOrigins),
-				handlers.AllowedHeaders(allowedHeaders),
-			)
-			handler = cors(handler)
+			corsOption := handlers.AllowedOrigins(c.AllowedOrigins)
+			handler = handlers.CORS(corsOption)(handler)
 		}
 		r.Handle(path.Join(issuerURL.Path, p), instrumentHandlerCounter(p, handler))
 	}
