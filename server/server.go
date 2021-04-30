b0825fb244d98ca63cad6123d666347a4cd1d45f
moomensaleem135 <moomensaleem135@gmail.com>
2021-04-30 18:31:49 +0400

    Add obsolete tokens, resolve conflicts, bump ent

--- server/server.go
+++ server/server.go
@@ -343,8 +343,7 @@ func newServer(ctx context.Context, c Config, rotationStrategy rotationStrategy)
 	handleFunc("/device", s.handleDeviceExchange)
 	handleFunc("/device/auth/verify_code", s.verifyUserCode)
 	handleFunc("/device/code", s.handleDeviceCode)
-	// TODO(nabokihms): "/device/token" endpoint is deprecated, consider using /token endpoint instead
-	handleFunc("/device/token", s.handleDeviceTokenDeprecated)
+	handleFunc("/device/token", s.handleDeviceToken)
 	handleFunc(deviceCallbackURI, s.handleDeviceCallback)
 	r.HandleFunc(path.Join(issuerURL.Path, "/callback"), func(w http.ResponseWriter, r *http.Request) {
 		// Strip the X-Remote-* headers to prevent security issues on
